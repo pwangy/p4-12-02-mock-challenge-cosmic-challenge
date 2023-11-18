0bcd1623c14ebca120bcd5fe6d41fe4f7f4a506f
Matteo Piccini <gnappo.1@live.it>
2023-11-17 16:30:20 -0800

    after lecture code

--- server/app.py
+++ server/app.py
@@ -7,13 +7,12 @@ from flask import Flask, make_response, jsonify, request
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -21,10 +20,25 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return ''
+    return ""
 
 
-if __name__ == '__main__':
+@app.route("/scientists")
+def scientists():
+    scientists = [
+        scientist.to_dict(rules=("-missions",)) for scientist in Scientist.query
+    ]
+    return make_response(scientists, 200)
+
+
+@app.route("/scientists/<int:id>")
+def scientist_by_id(id):
+    if scientist := db.session.get(Scientist, id):
+        return make_response(scientist.to_dict(), 200)
+    return make_response({"error": "Scientist not found"}, 404)
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -9,7 +9,7 @@ convention = {
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 
 metadata = MetaData(naming_convention=convention)
@@ -18,7 +18,7 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Planet(db.Model, SerializerMixin):
-    __tablename__ = 'planets'
+    __tablename__ = "planets"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -26,35 +26,78 @@ class Planet(db.Model, SerializerMixin):
     nearest_star = db.Column(db.String)
 
     # Add relationship
-
+    missions = db.relationship(
+        "Mission", back_populates="planet", cascade="all, delete-orphan"
+    )
+    scientists = association_proxy("missions", "scientist")
+    
     # Add serialization rules
+    serialize_rules = ("-missions.planet", "-scientists.planets")
 
 
 class Scientist(db.Model, SerializerMixin):
-    __tablename__ = 'scientists'
+    __tablename__ = "scientists"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     field_of_study = db.Column(db.String)
 
     # Add relationship
+    missions = db.relationship(
+        "Mission", back_populates="scientist", cascade="all, delete-orphan"
+    )
+    planets = association_proxy("missions", "planet")
 
     # Add serialization rules
+    serialize_rules = ("-missions.scientist", "-planets.scientists")
+
+    
+    @validates("name")
+    def validate_name(self, _, name):
+        if not name:
+            raise ValueError("Name has to be present")
+        return name
+
+    @validates("field_of_study")
+    def validate_field_of_study(self, _, field_of_study):
+        if not field_of_study:
+            raise ValueError("field_of_study has to be present")
+        return field_of_study
 
     # Add validation
 
 
 class Mission(db.Model, SerializerMixin):
-    __tablename__ = 'missions'
+    __tablename__ = "missions"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-
+    planet_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+    scientist_id = db.Column(db.Integer, db.ForeignKey("scientists.id"))
     # Add relationships
-
+    scientist = db.relationship("Scientist", back_populates="missions")
+    planet = db.relationship("Planet", back_populates="missions")
     # Add serialization rules
-
+    serialize_rules = ("-scientist.missions", "-planet.missions")
+    
     # Add validation
+    @validates("name")
+    def validate_name(self, _, name):
+        if not name:
+            raise ValueError("Name has to be present")
+        return name
+
+    @validates("scientist_id")
+    def validate_scientist_id(self, _, scientist_id):
+        if not scientist_id:
+            raise ValueError("scientist_id has to be present")
+        return scientist_id
+
+    @validates("planet_id")
+    def validate_planet_id(self, _, planet_id):
+        if not planet_id:
+            raise ValueError("planet_id has to be present")
+        return planet_id
 
 
 # add any models you may need.
